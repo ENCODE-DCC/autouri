e5deeaa8bfcf5bf126721f78ae791a74cdd084fe
Jin Lee <leepc12@gmail.com>
2020-03-13 17:37:23 -0700

    reduce nth for s3 race cond test

--- tests/test_race_cond.py
+++ tests/test_race_cond.py
@@ -17,6 +17,16 @@ S3URI:
 HTTPURL:
     Read-only storage so don't need to test
 
+Test nth threads competing to write on the same file v6.txt.
+Compare written string vs read string.
+
+Important notes:
+    Python API for GCS client() is not thread-safe.
+        So we need to specify thread_id here.
+        URIBase (and its child GCSURI) has a thread_id
+        This will make a new GCS client instance for each thread.
+    S3 Object Lock is based on Versioning.
+        We don't allow versioning so keep using unstable soft file lock.
 """
 import os
 import pytest
@@ -43,34 +53,30 @@ def write_v6_txt(x):
         assert u.read() == s
 
 
-def test_race_cond_autouri_write(
-    local_test_path,
-    gcs_test_path,
-    s3_test_path):
-    """Test nth threads competing to write on the same file v6.txt.
-    Compare written string vs read string.
+def run_write_v6_txt(prefix, nth):
+    s = os.path.join(prefix, 'v6.txt')
+    u = AutoURI(s)
+    if u.exists:
+        u.rm()
+    p = Pool(nth)
+    p.map(write_v6_txt, list(zip([s]*nth, range(nth))))
+    p.close()
+    p.join()
 
-    Important notes:
-        Python API for GCS client() is not thread-safe.
-            So we need to specify thread_id here.
-            URIBase (and its child GCSURI) has a thread_id
-            This will make a new GCS client instance for each thread.
-        S3 Object Lock is based on Versioning.
-            We don't allow versioning so keep using unstable soft file lock.
-    """
-    tests = (
-        (local_test_path, 50),
-        (gcs_test_path, 10),
-        (s3_test_path, 10),
-    )
-    # (local_test_path, gcs_test_path, s3_test_path,):
-    for (test_path, nth) in tests:
-        prefix = os.path.join(test_path, 'test_race_cond_autouri_write')
-        s = os.path.join(prefix, 'v6.txt')
-        u = AutoURI(s)
-        if u.exists:
-            u.rm()
-        p = Pool(nth)
-        p.map(write_v6_txt, list(zip([s]*nth, range(nth))))
-        p.close()
-        p.join()
+
+def test_race_cond_autouri_write_local(local_test_path):
+    prefix = os.path.join(local_test_path, 'test_race_cond_autouri_write_local')
+    nth = 50
+    run_write_v6_txt(prefix, nth)
+
+
+def test_race_cond_autouri_write_gcs(gcs_test_path):
+    prefix = os.path.join(gcs_test_path, 'test_race_cond_autouri_write_gcs')
+    nth = 10
+    run_write_v6_txt(prefix, nth)
+
+
+def test_race_cond_autouri_write_s3(s3_test_path):
+    nth = 5
+    prefix = os.path.join(s3_test_path, 'test_race_cond_autouri_write_s3')
+    run_write_v6_txt(prefix, nth)
