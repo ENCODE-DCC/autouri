ec1532ce0953e6be313b16fa234156feccddb73b
Jin wook Lee <leepc12@gmail.com>
2022-03-11 15:28:26 -0800

    better mocking time

--- tests/test_ntp_now.py
+++ tests/test_ntp_now.py
@@ -19,14 +19,13 @@ class MockedDataTime(datetime):
 
 
 def test_now_utc_wrong_os_time():
-    correct_now = datetime.now(timezone.utc)
-
+    reset_cached_offset()
     with patch("autouri.ntp_now.datetime", MockedDataTime):
-        reset_cached_offset()
+        ntp_now_utc = now_utc()
 
-        # should be accurate even though system time is 25 second behind NTP server time
-        assert abs((now_utc() - correct_now).total_seconds()) < 0.01
+    # should be accurate even though system time is 25 second behind NTP server time
+    assert abs((ntp_now_utc - datetime.now(timezone.utc)).total_seconds()) < 0.01
 
-        # cache offset should be 25 second
-        cached_offset_in_seconds = get_cached_offset().total_seconds()
-        assert cached_offset_in_seconds > 24.99 and cached_offset_in_seconds < 25.01
+    # cache offset should be 25 second
+    cached_offset_in_seconds = get_cached_offset().total_seconds()
+    assert cached_offset_in_seconds > 24.99 and cached_offset_in_seconds < 25.01
